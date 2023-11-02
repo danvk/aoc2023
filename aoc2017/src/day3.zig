6e68ac3d1bee7e0062e1f8613f1cf7d46a849622
Dan Vanderkam <danvdk@gmail.com>
2023-11-02 17:10:09 -0400

    very confusing hash map error

--- aoc2017/src/day3.zig
+++ aoc2017/src/day3.zig
@@ -63,6 +63,45 @@ fn part1(n: u32) u32 {
     return std.math.absCast(x) + std.math.absCast(y);
 }
 
+const Point = struct { x: i32, y: i32 };
+
+fn part2(n: u32) u32 {
+    var d = Dir.right;
+    var amount: u32 = 1;
+    var x: i32 = 0;
+    var y: i32 = 0;
+    var i: u32 = 1;
+
+    const values = std.AutoHashMap(Point, u32);
+
+    const ds = [_]i32{ -1, 0, 1 };
+
+    while (i < n) {
+        for (1..(1 + amount)) |_| {
+            var sum: u32 = 0;
+            for (ds) |dx| {
+                for (ds) |dy| {
+                    const pt = Point{ .x = (x + dx), .y = (y + dy) };
+                    const val = values.get(pt);
+                    sum += val orelse 0;
+                }
+            }
+            std.debug.print("{d},{d} {d} -> sum={d}\n", x, y, i, sum);
+            if (sum > n) {
+                return sum;
+            }
+            values.put(Point{ .x = x, .y = y }, sum);
+            i += 1;
+            x += d.dx();
+            y += d.dy();
+        }
+        d = d.next();
+        if (d == Dir.left or d == Dir.right) {
+            amount += 1;
+        }
+    }
+}
+
 pub fn main(allocator: std.mem.Allocator, args: []const [:0]u8) !void {
     _ = allocator;
     const arg = args[0];
@@ -71,7 +110,7 @@ pub fn main(allocator: std.mem.Allocator, args: []const [:0]u8) !void {
     const num = try std.fmt.parseInt(u32, arg, 10);
 
     std.debug.print("Part 1: {d}\n", .{part1(num)});
-    // std.debug.print("Part 2: {d}\n", .{sum2});
+    std.debug.print("Part 2: {d}\n", .{part2(num)});
 }
 
 test "next dir" {
