c701b0d7b8ee53e520288e1fd7aba5ad3448f357
Dan Vanderkam <danvdk@gmail.com>
2023-11-05 14:35:41 -0500

    struggling to write the type of InputStream

--- aoc2017/src/util.zig
+++ aoc2017/src/util.zig
@@ -12,28 +12,26 @@ pub fn readInts(line: []const u8, nums: *std.ArrayList(u32)) !void {
     }
 }
 
-fn LineIterator(comptime StreamType: type, in_stream: StreamType, file: std.fs.File, buf: std.ArrayList(u8)) type {
-    return struct {
-        file: std.fs.File = file,
-        in_stream: StreamType = in_stream,
-        buf: std.ArrayList(u8) = buf,
+const LineIterator = struct {
+    file: std.fs.File,
+    in_stream: std.io.Reader,
+    buf: std.ArrayList(u8),
 
-        pub fn next(self: *LineIterator) ?[]const u8 {
-            self.in_stream.readUntilDelimiterArrayList(&self.buf, '\n', 4096) catch |err| switch (err) {
-                error.EndOfStream => if (self.buf.items.len == 0) {
-                    return null;
-                },
-                else => |e| return e,
-            };
-            return self.buf.items;
-        }
+    pub fn next(self: *LineIterator) ?[]const u8 {
+        self.in_stream.readUntilDelimiterArrayList(&self.buf, '\n', 4096) catch |err| switch (err) {
+            error.EndOfStream => if (self.buf.items.len == 0) {
+                return null;
+            },
+            else => |e| return e,
+        };
+        return self.buf.items;
+    }
 
-        pub fn deinit(self: *LineIterator) void {
-            self.file.close();
-            self.buf.deinit(); // is this right, should it be allocator.free?
-        }
-    };
-}
+    pub fn deinit(self: *LineIterator) void {
+        self.file.close();
+        self.buf.deinit(); // is this right, should it be allocator.free?
+    }
+};
 
 pub fn iterLines(filename: []const u8, allocator: std.mem.Allocator) !LineIterator {
     // https://stackoverflow.com/a/68879352/388951
@@ -43,10 +41,9 @@ pub fn iterLines(filename: []const u8, allocator: std.mem.Allocator) !LineIterat
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
 
-    return LineIterator(
-        @TypeOf(in_stream),
-        file,
-        .in_stream,
-        std.ArrayList(u8).init(allocator),
-    );
+    return LineIterator{
+        .file = file,
+        .in_stream = in_stream,
+        .buf = std.ArrayList(u8).init(allocator),
+    };
 }
