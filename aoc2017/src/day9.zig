63893dce98eef88d039db99786eb4188638696d8
Dan Vanderkam <danvdk@gmail.com>
2023-11-07 10:39:58 -0500

    2017 day 9 star 2

--- aoc2017/src/day9.zig
+++ aoc2017/src/day9.zig
@@ -3,10 +3,11 @@ const util = @import("./util.zig");
 
 const assert = std.debug.assert;
 
-pub fn getScore(line: []const u8) u32 {
+pub fn scoreStream(line: []const u8) struct { score: u32, garbage: u32 } {
     var depth: u32 = 0;
     var in_garbage = false;
     var score: u32 = 0;
+    var num_garbage: u32 = 0;
     var i: usize = 0;
 
     while (i < line.len) {
@@ -21,6 +22,7 @@ pub fn getScore(line: []const u8) u32 {
                 },
                 else => {
                     // ignore everything else
+                    num_garbage += 1;
                 },
             }
         } else {
@@ -45,19 +47,27 @@ pub fn getScore(line: []const u8) u32 {
         i += 1;
     }
 
-    return score;
+    return .{ .score = score, .garbage = num_garbage };
+}
+
+fn getScore(line: []const u8) u32 {
+    return scoreStream(line).score;
+}
+
+fn getGarbage(line: []const u8) u32 {
+    return scoreStream(line).garbage;
 }
 
 pub fn main(allocator: std.mem.Allocator, args: []const [:0]u8) anyerror!void {
     const filename = args[0];
     const contents = try util.readInputFile(filename, allocator);
     defer allocator.free(contents);
-    std.debug.print("part1: {d}\n", .{getScore(contents)});
+    std.debug.print("part1/2: {any}\n", .{scoreStream(contents)});
 }
 
 const expectEqual = std.testing.expectEqual;
 
-test "samples" {
+test "samples part1" {
     try expectEqual(@as(u32, 1), getScore("{}"));
     // {{},{}}, score of 1 + 2 + 2 = 5.
     try expectEqual(@as(u32, 5), getScore("{{},{}}"));
@@ -76,4 +86,21 @@ test "samples" {
 
     // {{<a!>},{<a!>},{<a!>},{<ab>}}, score of 1 + 2 = 3.
     try expectEqual(@as(u32, 3), getScore("{{<a!>},{<a!>},{<a!>},{<ab>}}"));
+}
+
+test "samples part2" {
+    // <>, 0 characters.
+    try expectEqual(@as(u32, 0), getGarbage("<>"));
+    // <random characters>, 17 characters.
+    try expectEqual(@as(u32, 17), getGarbage("<random characters>"));
+    // <<<<>, 3 characters.
+    try expectEqual(@as(u32, 3), getGarbage("<<<<>"));
+    // <{!>}>, 2 characters.
+    try expectEqual(@as(u32, 2), getGarbage("<{!>}>"));
+    // <!!>, 0 characters.
+    try expectEqual(@as(u32, 0), getGarbage("<!!>"));
+    // <!!!>>, 0 characters.
+    try expectEqual(@as(u32, 0), getGarbage("<!!!>>"));
+    // <{o"i!a,<{i<a>, 10 characters.
+    try expectEqual(@as(u32, 10), getGarbage("<{o\"i!a,<{i<a>"));
 }
