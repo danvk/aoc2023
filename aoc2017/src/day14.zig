c1e713960a80a1fe8bc6dbcbce4310e3884e15d7
Dan Vanderkam <danvdk@gmail.com>
2023-11-13 10:10:49 -0500

    confusing error

--- aoc2017/src/day14.zig
+++ aoc2017/src/day14.zig
@@ -41,10 +41,37 @@ const Coord = struct {
     }
 };
 
+// caller is responsible for freeing hash map
+pub fn makeGrid(allocator: std.mem.Allocator, key: []const u8) !std.AutoHashMap(Coord, void) {
+    var grid = std.AutoHashMap(Coord, void).init(allocator);
+
+    var buf: [100]u8 = undefined;
+    var numSet: u32 = 0;
+    for (0..128) |row| {
+        const rowKey = try std.fmt.bufPrint(&buf, "{s}-{d}", .{ key, row });
+        const hash = try hashString(allocator, rowKey);
+        const y: i32 = @intCast(row);
+        std.debug.print("{d:>3} {b:0>128}\n", .{ y, hash });
+        for (0..128) |col| {
+            if (hash & (@as(u128, 1) << @as(u7, @intCast(127 - col))) == 1) {
+                const x: i32 = @intCast(col);
+                try grid.putNoClobber(Coord{ .y = y, .x = x }, undefined);
+                std.debug.print("{d}, {d}\n", .{ x, y });
+                numSet += 1;
+            }
+        }
+    }
+
+    std.debug.print("Num set: {d}\n", .{numSet});
+    return grid;
+}
+
 pub fn main(allocator: std.mem.Allocator, args: []const [:0]u8) anyerror!void {
     const key = args[0];
 
     std.debug.print("part 1: {d}\n", .{try part1(allocator, key)});
+    var grid = try makeGrid(allocator, key);
+    defer grid.deinit();
     // std.debug.print("part 2: {d}\n", .{part2(layers, maxLayer)});
 }
 
