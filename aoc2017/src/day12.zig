2a82c1df80ed98a0817217542ac1160b4e29b367
Dan Vanderkam <danvdk@gmail.com>
2023-11-14 12:05:11 -0500

    back to iterLines on day12

--- aoc2017/src/day12.zig
+++ aoc2017/src/day12.zig
@@ -84,23 +84,15 @@ pub fn main(in_allocator: std.mem.Allocator, args: []const [:0]u8) anyerror!void
     var allocator = arena.allocator();
 
     const filename = args[0];
-
-    var file = try std.fs.cwd().openFile(filename, .{});
-    defer file.close();
-
-    var buf_reader = std.io.bufferedReader(file.reader());
-    var in_stream = buf_reader.reader();
-
-    var buf: [1024]u8 = undefined;
-
-    // var line_it = try util.iterLines(filename, allocator);
-    // defer line_it.deinit();
+    var line_it = try util.iterLines(filename, allocator);
+    defer line_it.deinit();
 
     var programs = std.AutoHashMap(u32, Program).init(allocator);
     defer programs.deinit();
 
-    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        // std.debug.print("line: {s}\n", .{line});
+    // while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+    while (try line_it.next()) |line| {
+        std.debug.print("line: {s}\n", .{line});
         // Comment this out and the lines all look great:
         const program = try parseProgram(allocator, line);
         // std.debug.print("{any}\n", .{program});
