23b449dea04249cc001b2a327dab642794d5c44b
Dan Vanderkam <danvdk@gmail.com>
2023-11-21 11:11:35 -0500

    extractIntsIntoBuf

--- aoc2017/src/util.zig
+++ aoc2017/src/util.zig
@@ -1,15 +1,44 @@
 const std = @import("std");
 
 // Read u32s delimited by spaces or tabs from a line of text.
-pub fn readInts(comptime inttype: type, line: []const u8, nums: *std.ArrayList(inttype)) !void {
+pub fn readInts(comptime IntType: type, line: []const u8, nums: *std.ArrayList(IntType)) !void {
     var it = std.mem.splitAny(u8, line, ", \t");
     while (it.next()) |split| {
         if (split.len == 0) {
             continue;
         }
-        const num = try std.fmt.parseInt(inttype, split, 10);
+        const num = try std.fmt.parseInt(IntType, split, 10);
         try nums.append(num);
     }
+}
+
+fn isDigit(c: u8) bool {
+    return c == '-' or (c >= '0' and c <= '9');
+}
+
+pub fn extractIntsIntoBuf(comptime IntType: type, str: []const u8, buf: []IntType) ![]IntType {
+    var i: usize = 0;
+    var n: usize = 0;
+
+    while (i < str.len) {
+        const c = str[i];
+        if (isDigit(c)) {
+            const start = i;
+            i += 1;
+            while (i < str.len) {
+                const c2 = str[i];
+                if (!isDigit(c2)) {
+                    break;
+                }
+                i += 1;
+            }
+            buf[n] = try std.fmt.parseInt(IntType, str[start..i], 10);
+            n += 1;
+        } else {
+            i += 1;
+        }
+    }
+    return buf[0..n];
 }
 
 pub fn splitOne(line: []const u8, delim: []const u8) ?struct { head: []const u8, rest: []const u8 } {
@@ -104,6 +133,7 @@ pub fn readByLine(allocator: std.mem.Allocator, fileToClose: *std.fs.File, reade
     };
 }
 
+const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 const expectEqualDeep = std.testing.expectEqualDeep;
 
@@ -117,4 +147,18 @@ test "splitIntoBuf" {
     try expectEqualDeep(@as([]const u8, "gh12"), parts[3]);
     // const expected = [_][]const u8{ "abc", "def", "", "gh12" };
     // expectEqualDeep(@as([][]const u8, &[_][]const u8{ "abc", "def", "", "gh12" }), parts);
+}
+
+const eql = std.mem.eql;
+
+test "extractIntsIntoBuf" {
+    var buf: [8]i32 = undefined;
+    var ints = try extractIntsIntoBuf(i32, "12, 38, -233", &buf);
+    try expect(eql(i32, &[_]i32{ 12, 38, -233 }, ints));
+
+    ints = try extractIntsIntoBuf(i32, "zzz343344ddkd", &buf);
+    try expect(eql(i32, &[_]i32{343344}, ints));
+
+    ints = try extractIntsIntoBuf(i32, "not a number", &buf);
+    try expect(eql(i32, &[_]i32{}, ints));
 }
