99950e59067c8a3ba8304b90fc6a755342c00070
Dan Vanderkam <danvdk@gmail.com>
2023-11-22 09:55:13 -0500

    and another

--- aoc2017/day23/notes.js
+++ aoc2017/day23/notes.js
@@ -8,15 +8,13 @@ let c = 1000
 // 1000 iterations
 for (; b <= c; b += 17) {
     let f = 1
-    let d = 2
-    do {
+    for (let d = 2; d < b; d++) {
         for (let e = 2; e < b; e++) {
             if (d * e == b) {
                 f = 0
             }
         }
-        d += 1
-    } while (d != b);
+    }
     if (f == 0) {
         h += 1
     }
