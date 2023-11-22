8e0acdd4d039c634e93c02d01d772ae895322759
Dan Vanderkam <danvdk@gmail.com>
2023-11-22 09:57:13 -0500

    it's checking for isComposite

--- aoc2017/day23/notes.js
+++ aoc2017/day23/notes.js
@@ -5,12 +5,10 @@ let h = 0;
 let b = 93
 let c = 1000
 
-function check(b) {
+function isComposite(b) {
     for (let d = 2; d < b; d++) {
-        for (let e = 2; e < b; e++) {
-            if (d * e == b) {
-                return true;
-            }
+        if (b % d == 0) {
+            return true;
         }
     }
     return false;
@@ -18,7 +16,7 @@ function check(b) {
 
 // 1000 iterations
 for (; b <= c; b += 17) {
-    if (check(b)) {
+    if (isComposite(b)) {
         h += 1
     }
 }
