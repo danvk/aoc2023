e9f16af33a7434c154739b2cc93b5bf45b2658ba
Dan Vanderkam <danvdk@gmail.com>
2023-11-22 09:55:58 -0500

    factor out function

--- aoc2017/day23/notes.js
+++ aoc2017/day23/notes.js
@@ -5,17 +5,20 @@ let h = 0;
 let b = 93
 let c = 1000
 
-// 1000 iterations
-for (; b <= c; b += 17) {
-    let f = 1
+function check(b) {
     for (let d = 2; d < b; d++) {
         for (let e = 2; e < b; e++) {
             if (d * e == b) {
-                f = 0
+                return true;
             }
         }
     }
-    if (f == 0) {
+    return false;
+}
+
+// 1000 iterations
+for (; b <= c; b += 17) {
+    if (check(b)) {
         h += 1
     }
 }
