92da591bf98191aa2f0c0421a3223de0eb8e0773
Dan Vanderkam <danvdk@gmail.com>
2023-12-01 08:09:30 -0500

    2023 day 1 star 2

--- aoc2017/src/2023day1.zig
+++ aoc2017/src/2023day1.zig
@@ -3,6 +3,8 @@ const bufIter = @import("./buf-iter.zig");
 
 const assert = std.debug.assert;
 
+const NUMS = [_][]const u8{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
 pub fn main(allocator: std.mem.Allocator, args: []const [:0]u8) anyerror!void {
     _ = allocator;
     const filename = args[0];
@@ -12,15 +14,26 @@ pub fn main(allocator: std.mem.Allocator, args: []const [:0]u8) anyerror!void {
     while (try iter.next()) |line| {
         var firstInt: ?u8 = null;
         var lastInt: ?u8 = null;
-        for (line) |c| {
-            if (c < '0' or c > '9') {
-                continue;
+        for (line, 0..) |c, i| {
+            var digit: ?u8 = null;
+            if (c >= '0' and c <= '9') {
+                digit = c - '0';
+            } else {
+                for (NUMS, 1..) |numStr, d| {
+                    const lineSlice = line[i..@min(i + numStr.len, line.len)];
+                    if (std.mem.eql(u8, numStr, lineSlice)) {
+                        digit = @intCast(d);
+                    }
+                }
             }
-            var digit = c - '0';
-            if (firstInt == null) {
-                firstInt = digit;
+
+            if (digit) |d| {
+                _ = d;
+                if (firstInt == null) {
+                    firstInt = digit;
+                }
+                lastInt = digit;
             }
-            lastInt = digit;
         }
         std.debug.print("{s} -> {d} / {d}\n", .{ line, firstInt.?, lastInt.? });
         totalSum += 10 * firstInt.? + lastInt.?;
