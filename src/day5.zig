5e29e788d9a97863e5b338c2879ba16c6372ca82
Dan Vanderkam <danvdk@gmail.com>
2023-12-05 15:02:40 -0500

    three-way split for intervals

--- src/day5.zig
+++ src/day5.zig
@@ -21,6 +21,20 @@ fn mapThroughRanges(num: u64, ranges: []Range) u64 {
     return num;
 }
 
+fn mapRangeThroughRange(r: Iv64, range: Range) struct { mapped: ?Iv64, remaining: ?Iv64 } {
+    const src = range.source;
+    if (!src.intersects(r)) {
+        return .{ .mapped = null, .remaining = src };
+    }
+    if (src.contains(r)) {
+        const mapped = Iv64{
+            .low = range.dest + (r.low - src.low),
+            .high = range.dest + (r.high - src.low),
+        };
+        return .{ .mapped = mapped, .remaining = null };
+    }
+}
+
 fn readRanges(alloc: std.mem.Allocator, iter: *bufIter.ReadByLineIterator) !std.ArrayList(Range) {
     var out = std.ArrayList(Range).init(alloc);
     var intBuf: [3]u64 = undefined;
@@ -55,20 +69,15 @@ pub fn main(in_allocator: std.mem.Allocator, args: []const [:0]u8) anyerror!void
     assert(seeds.len > 2);
     _ = try iter.next();
 
-    // var numSeeds: u64 = 0;
-    // _ = numSeeds;
-    // var i: usize = 0;
-    // var part2Seeds = std.ArrayList(u64).init(allocator);
+    var part2Seeds = std.ArrayList(Iv64).init(allocator);
     // defer part2Seeds.deinit();
-    // while (i < seeds.len) : (i += 2) {
-    //     var start = seeds[i];
-    //     var num = seeds[i + 1];
-    //     for (start..(start + num)) |s| {
-    //         try part2Seeds.append(s);
-    //     }
-    // }
-    // var seeds2 = part2Seeds.items;
-    // std.debug.print("part 2: {d} seeds\n", .{seeds2.len});
+    var i: usize = 0;
+    while (i < seeds.len) : (i += 2) {
+        var start = seeds[i];
+        var num = seeds[i + 1];
+        try part2Seeds.append(Iv64{ .low = start, .high = start + num });
+    }
+    var seeds2 = part2Seeds.items;
 
     while (try iter.next()) |line| {
         assert(std.mem.endsWith(u8, line, "map:"));
@@ -81,6 +90,11 @@ pub fn main(in_allocator: std.mem.Allocator, args: []const [:0]u8) anyerror!void
             seeds[j] = mapThroughRanges(seeds[j], ranges.items);
         }
 
+        var newRanges = std.ArrayList(Iv64).init(allocator);
+        _ = newRanges;
+        for (seeds2) |seedRange| {
+            _ = seedRange;
+        }
         // j = 0;
         // while (j < seeds2.len) : (j += 1) {
         //     seeds2[j] = mapThroughRanges(seeds2[j], ranges.items);
--- src/interval.zig
+++ src/interval.zig
@@ -20,6 +20,50 @@ pub fn Interval(comptime IntType: type) type {
             return val >= self.low and val < self.high;
         }
 
+        // Does self fully contain other?
+        pub fn contains(self: @This(), other: @This()) bool {
+            return (self.low <= other.low and self.high >= other.high);
+        }
+
+        const Self = @This();
+
+        // Split self into intersecting and non-intersecting bits according to other
+        pub fn split(self: Self, other: Self) struct { int: ?Self, pre: ?Self, post: ?Self } {
+            // does not intersect
+            if (self.high <= other.low) {
+                return .{ .pre = self, .int = null, .post = null };
+            } else if (self.low >= other.high) {
+                return .{ .pre = null, .int = null, .post = self };
+            }
+            // complete containment
+            if (other.contains(self)) {
+                return .{ .int = self, .pre = null, .post = null };
+            } else if (self.contains(other)) {
+                return .{
+                    .int = other,
+                    .pre = if (self.low == other.low) null else Self{ .low = self.low, .high = other.low },
+                    .post = if (other.high == self.high) null else Self{ .low = other.high, .high = self.high },
+                };
+            }
+            // overlap
+            if (self.low < other.low) {
+                assert(other.low > self.low);
+                return .{
+                    .int = self.intersection(other),
+                    .pre = Self{ .low = self.low, .high = other.low },
+                    .post = null,
+                };
+            } else if (self.low > other.low) {
+                assert(other.high < self.high);
+                return .{
+                    .int = self.intersection(other),
+                    .pre = null,
+                    .post = Self{ .low = other.high, .high = self.high },
+                };
+            }
+            unreachable;
+        }
+
         pub fn len(self: @This()) IntType {
             return self.high - self.low;
         }
@@ -61,4 +105,22 @@ test "interval contains and length" {
     assert(!a.includes(20));
     assert(!a.includes(0));
     try expectEqual(a.len(), 10);
+}
+
+test "interval three-way split" {
+    const a = Iv32{ .low = 10, .high = 20 };
+    const b = Iv32{ .low = 15, .high = 25 };
+
+    try expectDeepEqual(a.split(b), .{ .int = Iv32{ .low = 15, .high = 20 }, .pre = Iv32{ .low = 10, .high = 15 }, .post = null });
+    try expectDeepEqual(b.split(a), .{ .int = Iv32{ .low = 15, .high = 20 }, .post = Iv32{ .low = 20, .high = 25 }, .pre = null });
+
+    const big = Iv32{ .low = 5, .high = 25 };
+    try expectDeepEqual(b.split(big), .{ .int = b, .pre = null, .post = null });
+    try expectDeepEqual(a.split(big), .{ .int = a, .pre = null, .post = null });
+    try expectDeepEqual(big.split(a), .{ .int = a, .pre = Iv32{ .low = 5, .high = 10 }, .post = Iv32{ .low = 20, .high = 25 } });
+    try expectDeepEqual(big.split(b), .{ .int = b, .pre = Iv32{ .low = 5, .high = 15 }, .post = null });
+
+    const c = Iv32{ .low = 20, .high = 30 };
+    try expectDeepEqual(a.split(c), .{ .int = null, .pre = a, .post = null });
+    try expectDeepEqual(c.split(a), .{ .int = null, .pre = null, .post = c });
 }
