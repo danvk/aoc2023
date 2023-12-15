24bff954ecf0c026376193f0ebce4ca46431d92f
Dan Vanderkam <danvdk@gmail.com>
2023-12-15 14:14:54 -0600

    hashfn

---
+++ src/day15.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+const bufIter = @import("./buf-iter.zig");
+const util = @import("./util.zig");
+
+const assert = std.debug.assert;
+
+// alternative with arena allocator:
+// pub fn main(in_allocator: std.mem.Allocator, args: []const [:0]u8) anyerror!void {
+//     var arena = std.heap.ArenaAllocator.init(in_allocator);
+//     defer arena.deinit();
+//     var allocator = arena.allocator();
+
+// Determine the ASCII code for the current character of the string.
+// Increase the current value by the ASCII code you just determined.
+// Set the current value to itself multiplied by 17.
+// Set the current value to the remainder of dividing itself by 256.
+fn hash(str: []const u8) u8 {
+    var val: u16 = 0;
+    for (str) |c| {
+        val += c;
+        val *= 17;
+        val = val & 0xff;
+    }
+    return @intCast(val);
+}
+
+pub fn main(allocator: std.mem.Allocator, args: []const [:0]u8) anyerror!void {
+    _ = allocator;
+    const filename = args[0];
+
+    var iter = try bufIter.iterLines(filename);
+    while (try iter.next()) |line| {
+        _ = line;
+    }
+
+    // std.debug.print("part 1: {d}\n", .{sum1});
+    // std.debug.print("part 2: {d}\n", .{sum2});
+}
+
+const expectEqualDeep = std.testing.expectEqualDeep;
+const expectEqual = std.testing.expectEqual;
+const expect = std.testing.expect;
+
+test "hash" {
+    try expectEqual(hash("HASH"), 52);
+}
