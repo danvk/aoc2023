ba784b3d55c4fff8007008946e9a1d503e7ce30b
Dan Vanderkam <danvdk@gmail.com>
2023-12-15 14:50:57 -0600

    day 15 star 2

--- src/day15.zig
+++ src/day15.zig
@@ -24,11 +24,21 @@ fn hash(str: []const u8) u8 {
     return @intCast(val);
 }
 
-pub fn main(allocator: std.mem.Allocator, args: []const [:0]u8) anyerror!void {
+const Lens = struct { label: []const u8, focalLen: u8 };
+
+fn printLens(lens: Lens) void {
+    std.debug.print("[{s} {d}] ", .{ lens.label, lens.focalLen });
+}
+
+pub fn main(in_allocator: std.mem.Allocator, args: []const [:0]u8) anyerror!void {
+    var arena = std.heap.ArenaAllocator.init(in_allocator);
+    defer arena.deinit();
+    var allocator = arena.allocator();
     const filename = args[0];
 
     var iter = try bufIter.iterLines(filename);
     var sum: u64 = 0;
+    var sum2: u64 = 0;
     while (try iter.next()) |line| {
         var parts = std.ArrayList([]const u8).init(allocator);
         defer parts.deinit();
@@ -39,10 +49,77 @@ pub fn main(allocator: std.mem.Allocator, args: []const [:0]u8) anyerror!void {
             const v = hash(part);
             sum += v;
         }
+
+        var boxes: [256]std.ArrayList(Lens) = undefined;
+        for (0..256) |i| {
+            boxes[i] = std.ArrayList(Lens).init(allocator);
+        }
+
+        for (parts.items) |part| {
+            std.debug.print("After \"{s}\"\n", .{part});
+
+            var partsBuf: [2][]const u8 = undefined;
+            var partParts = util.splitAnyIntoBuf(part, "-=", &partsBuf);
+            var label = partParts[0];
+            const v = hash(label);
+            if (std.mem.endsWith(u8, part, "-")) {
+                var maybeIdx: ?usize = null;
+                var items = boxes[v].items;
+                for (items, 0..) |lens, i| {
+                    if (std.mem.eql(u8, lens.label, label)) {
+                        maybeIdx = i;
+                        break;
+                    }
+                }
+                if (maybeIdx) |idx| {
+                    for (idx..items.len - 1) |i| {
+                        items[i] = items[i + 1];
+                    }
+                    _ = boxes[v].pop();
+                }
+            } else if (part[part.len - 2] == '=') {
+                // insert
+                var focalLen = try std.fmt.parseInt(u8, part[part.len - 1 ..], 10);
+                var items = boxes[v].items;
+                var maybeIdx: ?usize = null;
+                for (items, 0..) |lens, i| {
+                    if (std.mem.eql(u8, lens.label, label)) {
+                        maybeIdx = i;
+                        break;
+                    }
+                }
+                const lens = Lens{ .label = label, .focalLen = focalLen };
+                if (maybeIdx) |idx| {
+                    items[idx] = lens;
+                } else {
+                    try boxes[v].append(lens);
+                }
+            } else {
+                unreachable;
+            }
+            for (boxes, 0..) |box, i| {
+                if (box.items.len > 0) {
+                    std.debug.print("Box {d}: ", .{i});
+                    for (box.items) |item| {
+                        printLens(item);
+                    }
+                    std.debug.print("\n", .{});
+                }
+            }
+            std.debug.print("\n", .{});
+        }
+
+        for (boxes, 1..) |box, boxNum| {
+            for (box.items, 1..) |lens, slotNum| {
+                var power = boxNum * slotNum * lens.focalLen;
+                std.debug.print("{s}: {d} * {d} * {d} = {d}\n", .{ lens.label, boxNum, slotNum, lens.focalLen, power });
+                sum2 += power;
+            }
+        }
     }
 
     std.debug.print("part 1: {d}\n", .{sum});
-    // std.debug.print("part 2: {d}\n", .{sum2});
+    std.debug.print("part 2: {d}\n", .{sum2});
 }
 
 const expectEqualDeep = std.testing.expectEqualDeep;
