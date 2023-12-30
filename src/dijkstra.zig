8a2338cf97e7e84559a4648524cc81cd4626fea2
Dan Vanderkam <danvdk@gmail.com>
2023-12-30 16:48:55 -0500

    trying to implement shortest path

--- src/dijkstra.zig
+++ src/dijkstra.zig
@@ -54,3 +54,114 @@ pub fn dijkstra(
     }
     return null;
 }
+
+pub fn WithCostAndPrev(comptime State: type) type {
+    return struct {
+        state: State,
+        cost: u32,
+        prev: ?*WithCostAndPrev(State),
+    };
+}
+
+fn stateCostPrevOrder(comptime State: type) fn (void, *WithCostAndPrev(State), *WithCostAndPrev(State)) std.math.Order {
+    return struct {
+        pub fn inner(_: void, a: *WithCostAndPrev(State), b: *WithCostAndPrev(State)) std.math.Order {
+            return std.math.order(a.cost, b.cost);
+        }
+    }.inner;
+}
+
+pub fn shortestPath(
+    comptime State: type, //
+    in_allocator: std.mem.Allocator,
+    context: anytype,
+    start: State, //
+    neighbors: fn (@TypeOf(context), WithCost(State), *std.ArrayList(WithCost(State))) std.mem.Allocator.Error!void, //
+    dest: State,
+) !?[]WithCost(State) {
+    var arena = std.heap.ArenaAllocator.init(in_allocator);
+    defer arena.deinit();
+    var allocator = arena.allocator();
+
+    const StateWithCost = WithCost(State);
+    const StateWithCostPrev = WithCostAndPrev(State);
+
+    var seen = std.AutoHashMap(State, u32).init(allocator);
+    defer seen.deinit();
+
+    var fringe = std.PriorityQueue(*StateWithCostPrev, void, stateCostPrevOrder(State)).init(allocator, {});
+    defer fringe.deinit();
+
+    var seed = StateWithCostPrev{ .cost = 0, .state = start, .prev = null };
+    try fringe.add(&seed);
+
+    while (fringe.removeOrNull()) |stateCostPrevPtr| {
+        const state = stateCostPrevPtr.state;
+        const cost = stateCostPrevPtr.cost;
+        if (seen.get(state)) |prev_cost| {
+            if (prev_cost <= cost) {
+                continue;
+            }
+        }
+
+        if (std.meta.eql(dest, state)) {
+            var path = std.ArrayList(StateWithCost).init(in_allocator);
+            var curPtr: ?*StateWithCostPrev = stateCostPrevPtr;
+            while (curPtr) |cur| {
+                try path.append(StateWithCost{ .state = cur.state, .cost = cur.cost });
+                curPtr = cur.prev;
+            }
+            std.mem.reverse(StateWithCost, path.items);
+            return try path.toOwnedSlice();
+        }
+
+        try seen.put(state, cost);
+        var nexts = std.ArrayList(StateWithCost).init(in_allocator);
+        defer nexts.deinit();
+        try neighbors(context, StateWithCost{ .state = state, .cost = cost }, &nexts);
+        var nextPrevs = try std.ArrayList(*StateWithCostPrev).initCapacity(in_allocator, nexts.items.len);
+        defer nextPrevs.deinit();
+        for (nexts.items) |next| {
+            var nextPrev = try allocator.create(StateWithCostPrev);
+            nextPrev.* = StateWithCostPrev{
+                .cost = next.cost,
+                .state = next.state,
+                .prev = stateCostPrevPtr,
+            };
+            try nextPrevs.append(nextPrev);
+        }
+        try fringe.addSlice(nextPrevs.items);
+    }
+    return null;
+}
+
+const StrWithCost = WithCost([]const u8);
+fn graph_neighbors(
+    g: std.StringHashMap(std.ArrayList([]const u8)),
+    node: StrWithCost,
+    out: *std.ArrayList(StrWithCost),
+) !void {
+    if (g.get(node.state)) |nexts| {
+        const cost = node.cost;
+        for (nexts.items) |n| {
+            try out.append(StrWithCost{ .state = n, .cost = cost + 1 });
+        }
+    }
+}
+
+test "shortest path" {
+    var g = std.StringHashMap(std.ArrayList([]const u8)).init(std.testing.allocator);
+    defer g.deinit();
+    var an = std.ArrayList([]const u8).init(std.testing.allocator);
+    defer an.deinit();
+    try an.append("B");
+    try an.append("C");
+    try g.put("A", an);
+
+    var bn = std.ArrayList([]const u8).init(std.testing.allocator);
+    defer bn.deinit();
+    try bn.append("D");
+
+    var path = try shortestPath([]const u8, std.testing.allocator, g, "A", graph_neighbors, "D");
+    std.debug.print("path: {any}\n", .{path});
+}
